9230b910f28fae3fd47bf183b434b9fea18344b1
Sam Habiel <sam.habiel@gmail.com>
2013-10-18 09:38:17 -0700

    Added % as general calling method for any HTTP verb; fixed leaking variable I

--- src/_WC.m
+++ src/_WC.m
@@ -1,5 +1,73 @@
-%WC ; VEN/SMH - Web Services Client using cURL ;2013-06-18  9:50 AM
+%WC ; VEN/SMH - Web Services Client using cURL ;2013-10-18  9:34 AM
  ; See accompanying License for terms of use.
+ ;
+%(RETURN,METHOD,URL,PAYLOAD,MIME,TO,HEADERS) ; General call for any type
+ ;
+ ;
+ ; DEBUG; Test error trapping.
+ ; N X S X=1/0
+ ; DEBUG
+ ;
+ ;
+ S TO=$G(TO) ; Timeout
+ I +TO=0 S TO=30 ; Default timeout
+ ;
+ ; Write payload to File in shared memory
+ N F S F="/dev/shm/"_$R(987987234)_$J_".DAT"
+ O F:(NEWVERSION) U F
+ I $D(PAYLOAD)#2 W PAYLOAD,!
+ N I F I=0:0 S I=$O(PAYLOAD(I)) Q:'I  W PAYLOAD(I),!
+ C F
+ ;
+ ; Flags: -s : Silent; -X: HTTP POST; -k : Ignore certificate validation.
+ ; --connect-timeout: try only for this long; -m: max time to try. Both in sec.
+ ; -i: Print headers out in response.
+ N CMD S CMD="curl -si -X"_METHOD_" --connect-timeout "_TO_" -m "_TO_" -k "_URL_" --header 'Content-Type:"_MIME_"'"_" --data-binary @"_F
+ ;
+ ; DEBUG ; See if we can get an error if curl isn't found on the Operating System.
+ ;N CMD S CMD="curly -si -XPOST --connect-timeout "_TO_" -m "_TO_" -k "_URL_" --header 'Content-Type:"_MIME_"'"_" --data-binary @"_F
+ ; DEBUG
+ ;
+ ; DEBUG
+ ; W !,CMD
+ ; DEBUG
+ ;
+ ; TODO: Check curl return status. VEN/SMH - Seems that there is no way to get that from GT.M right now.
+ ; VEN/SMH - confirmed with Bhaskar that GT.M doesn't have a way check return status.
+ ;
+ ; VEN/SMH Okay. This the code is hard to understand. See comments.
+ ;
+ ; Execute and read back
+ N D S D="cURLDevice"
+ O D:(shell="/bin/sh":command=CMD:PARSE)::"PIPE" U D
+ ;
+ ; Flag to indicate whether a line we are getting a header or not. We are getting headers first, so it's true.
+ ; A la State machine.
+ N ISHEADER S ISHEADER=1 
+ N I F I=1:1 R RETURN(I)#4000:1 Q:$ZEOF  D   ; Read each line up to 4000 characters
+ . S RETURN(I)=$$TRIM(RETURN(I),,$C(13)) ; Strip CRs (we are on Unix)
+ . I RETURN(I)="",$G(HEADERS("STATUS")) S ISHEADER=0  ; If we get a blank line, and we don't have a status yet (e.g. if we got a 100 which we kill off), we are no longer at the headers
+ . I ISHEADER D  QUIT                    ; If we are at the headers, read them & remove them from RETURN array.
+ . . ; First Line is like HTTP/1.1 200 OK
+ . . I RETURN(I)'[":" S HEADERS("PROTOCOL")=$P(RETURN(I)," "),HEADERS("STATUS")=$P(RETURN(I)," ",2) K RETURN(I)
+ . . ; Next lines are key: value pairs. 
+ . . E  S HEADERS($P(RETURN(I),":"))=$$TRIM($P(RETURN(I),":",2,99)) K RETURN(I)
+ . . I HEADERS("STATUS")=100 K HEADERS("PROTOCOL"),HEADERS("STATUS") QUIT  ; We don't want the continue
+ . K:RETURN(I)="" RETURN(I) ; remove empty line
+ K:RETURN(I)="" RETURN(I)  ; remove empty line (last line when $ZEOF gets hit)
+ C D
+ 
+ ; Delete the file a la %ZISH
+ O F C F:(DELETE)
+ ;
+ ; Comment the zwrites out to see the return vales from the function
+ ;DEBUG
+ ; ZWRITE HEADERS
+ ; ZWRITE RETURN
+ ;DEBUG
+ ;
+ QUIT
+ ;
  ;
 POST(RETURN,URL,PAYLOAD,MIME,TO,HEADERS) ; Post
  ;D EWD(.RETURN,URL,.PAYLOAD,MIME)
@@ -24,7 +92,7 @@ CURL(RETURN,URL,PAYLOAD,MIME,TO,HEADERS) ; Post using CURL
  N F S F="/dev/shm/"_$R(987987234)_$J_".DAT"
  O F:(NEWVERSION) U F
  I $D(PAYLOAD)#2 W PAYLOAD,!
- F I=0:0 S I=$O(PAYLOAD(I)) Q:'I  W PAYLOAD(I),!
+ N I F I=0:0 S I=$O(PAYLOAD(I)) Q:'I  W PAYLOAD(I),!
  C F
  ;
  ; Flags: -s : Silent; -X: HTTP POST; -k : Ignore certificate validation.
@@ -52,7 +120,7 @@ CURL(RETURN,URL,PAYLOAD,MIME,TO,HEADERS) ; Post using CURL
  ; Flag to indicate whether a line we are getting a header or not. We are getting headers first, so it's true.
  ; A la State machine.
  N ISHEADER S ISHEADER=1 
- F I=1:1 R RETURN(I)#4000:1 Q:$ZEOF  D   ; Read each line up to 4000 characters
+ N I F I=1:1 R RETURN(I)#4000:1 Q:$ZEOF  D   ; Read each line up to 4000 characters
  . S RETURN(I)=$$TRIM(RETURN(I),,$C(13)) ; Strip CRs (we are on Unix)
  . I RETURN(I)="",$G(HEADERS("STATUS")) S ISHEADER=0  ; If we get a blank line, and we don't have a status yet (e.g. if we got a 100 which we kill off), we are no longer at the headers
  . I ISHEADER D  QUIT                    ; If we are at the headers, read them & remove them from RETURN array.
