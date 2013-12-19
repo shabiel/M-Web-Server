e0befc183acff8043299433177c5ac75ad9507e2
Sam Habiel <sam.habiel@gmail.com>
2013-12-18 16:15:45 -0800

    Routine to automatically map %W namespaces away from %SYS in Cache

---
+++ src/WWWINIT.m
@@ -0,0 +1,20 @@
+WWWINIT ; VEN/SMH - Initialize %W namespaces for Cache ; 12/18/13 12:08pm
+ ;;0.1;MASH WEB SERVER/WEB SERVICES
+ ;
+ ; Get current namespace
+ N NMSP S NMSP=$ZU(5)
+ ;
+ ; Map %W globals away from %SYS
+ ZN "%SYS" ; Go to SYS
+ N % S %=##class(Config.Configuration).GetGlobalMapping(NMSP,"%W*","",NMSP,NMSP)
+ I '% S %=##class(Config.Configuration).AddGlobalMapping(NMSP,"%W*","",NMSP,NMSP)
+ I '% W !,"Error="_$SYSTEM.Status.GetErrorText(%) QUIT
+ ;
+ ; Map %W routines away from %SYS
+ N A S A("Database")=NMSP
+ N % S %=##Class(Config.MapRoutines).Get(NMSP,"%W*",.A)
+ S A("Database")=NMSP
+ I '% S %=##Class(Config.MapRoutines).Create(NMSP,"%W*",.A)
+ I '% W !,"Error="_$SYSTEM.Status.GetErrorText(%) QUIT
+ ZN NMSP ; Go back
+ QUIT
